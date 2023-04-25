e747fc6668f1c1e03882fa716c66fb370b6a6edb
David Radjai <dradjai@yahoo.com>
2023-04-25 14:33:46 -0400

    latest typescript work

---
+++ index.ts
@@ -0,0 +1,44 @@
+let age: number = 23;
+let firstName: string = "David";
+let isTall: boolean = true;
+let childrenNames: string[] = ["Lily", "David"]; 
+
+console.log(`${firstName} is ${age} years old`);
+
+if(isTall) {
+  console.log('and so tall')
+}
+else {
+  console.log('and so pretty');
+}
+
+for(let i: number = 0; i < childrenNames.length; i++) {
+  console.log(childrenNames[i]);
+}
+
+enum taxForm {
+  standardTaxForm = "1040",
+  childTaxForm = "641",
+  cryptoTaxForm = "420S"
+}
+
+// console.log("the crypto one is: ", taxForm.cryptoTaxForm);
+// console.log("the crypto one is: ", taxForm.cryptoTaxForm);
+// console.log("the crypto one is: ", taxForm.cryptoTaxForm);
+// console.log("the crypto one is: ", taxForm.cryptoTaxForm);
+
+let zipCode: string | number = "12345";
+zipCode = "ABCDE";
+zipCode = 123;
+
+// console.log(zipCode);
+
+function sum(x: number, y: number) : number {
+  return x + y;
+
+}
+
+let total: number = sum(5, 6);
+console.log(total);
+
+
